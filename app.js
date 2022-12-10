aa2d93a9108f6301b5768dfd09263a8e135872a7
Abhishek Choudhari <abhishekc97@gmail.com>
2022-12-10 20:45:37 +0530

    added create a category api

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-dotenv.config();
-const _ = require("lodash");
+const dotenv = require("dotenv").config();
 const mongodb = require("./config/mongodb.js");
 
 const port = process.env.PORT || 3000;
@@ -26,69 +24,45 @@ app.get("/api/health", function (req, res) {
     res.send(`Backend server is active as of time: ${new Date()}`);
 });
 
-// schema of a single gallery category
-const galleryCategorySchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    createdAt: String,
-    updatedAt: String,
-});
-
 // schema of an image
-const imageGallerySchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    createdAt: Date,
-    updatedAt: Date,
-    category: [galleryCategorySchema],
-    likes: Number,
-    imageLink: String,
-});
+const imageSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        category: { type: Array, default: [] },
+        likes: { type: Number, default: 0 },
+        imageLink: { type: String },
+    },
+    { timestamps: true }
+);
 
-const ImageGallery = mongoose.model("ImageGallery", imageGallerySchema);
+const Image = mongoose.model("Image", imageSchema);
+
+// schema of a single gallery category
+const galleryCategorySchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+    },
+    { timestamps: true }
+);
+
 const GalleryCategory = mongoose.model(
     "GalleryCategory",
     galleryCategorySchema
 );
 
-const category1 = new GalleryCategory({
-    name: "category-1",
-});
-const category2 = new GalleryCategory({
-    name: "category-2",
-});
-const category3 = new GalleryCategory({
-    name: "category-3",
-});
-const category4 = new GalleryCategory({
-    name: "category-4",
-});
-
-defaultCategories = [category1, category2, category3, category4];
-
-app.get("/admin", function (req, res) {
-    GalleryCategory.find({}, function (err, foundCategories) {
-        if (foundCategories.length === 0) {
-            GalleryCategory.insertMany(defaultCategories, function (err) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log("Added default categories");
-                }
-            });
-            res.send("Added default categories");
+// POST request for creating a category
+app.post("/admin/addCategory", function (req, res) {
+    const newCategory = new GalleryCategory({
+        name: req.body.name,
+    });
+    newCategory.save(function (err, newCategory) {
+        if (err) {
+            console.log(err);
         } else {
-            res.send(foundCategories);
+            console.log(newCategory + "saved this category.");
         }
     });
-});
 
-app.post("/admin/createCategory", function (req, res) {
-    const customCategory = req.body.customCategory;
-
-    const newCategory = new GalleryCategory({
-        name: customCategory,
-        createdAt: new Date(),
-    });
-    newCategory.save();
     console.log("New category added");
     res.send("New category added");
 });
