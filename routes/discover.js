1040a3119dcbaba66f94c27e5769625a50f2dd30
Abhishek Choudhari <abhishekc97@gmail.com>
2022-12-11 22:27:25 +0530

    made discover route

--- routes/discover.js
+++ routes/discover.js
@@ -17,8 +17,18 @@ route.get("/categories", function (req, res, next) {
 });
 
 /** API to find images by a category */
+/** API to filter image results http://localhost:3000/api/images/:categoryName?sortByDate=asc&sortByLikes=10 */
+
 route.get("/images/:categoryName", function (req, res, next) {
     const categoryName = req.params.categoryName;
+
+    /** get query parameters */
+    const queryParams = req.query;
+    console.log(queryParams);
+    const sortByDate = queryParams.sortByDate;
+    const sortByLikes = queryParams.sortByLikes;
+    // let parameters = new URLSearchParams(window.location.search);
+
     const imageSet = [];
     Image.find(
         { category: categoryName },
@@ -31,13 +41,15 @@ route.get("/images/:categoryName", function (req, res, next) {
             /** important to return res to the callback here itself and not outside find function */
         }
     ).limit(4);
+    // .sort(sortByDate)
+    // .sort(sortByLikes);
 });
 
-route.get("/images/:category:filter", function (req, res, next) {
-    const category = req.params.category;
-    const filter = req.params.filter;
-
-    console.log(category + " " + filter);
+/** API to shuffle the image search results */
+route.get("/images/:category/:shuffle", function (req, res, next) {
+    const categoryName = req.params.categoryName;
+    const shuffle = req.params.shuffle;
+    const imageSet = [];
 });
 
 module.exports = route;
