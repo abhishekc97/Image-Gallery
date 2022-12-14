9879ee528a32acf789cc55061b71a799873da6d7
Abhishek Choudhari <abhishekc97@gmail.com>
2022-12-14 19:16:37 +0530

    added filters to discover api

--- routes/discover.js
+++ routes/discover.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const route = Router();
 
 const GalleryCategory = require("../models/GalleryCategory");
-const Image = require("../models/ImageGallery");
+const ImageGallery = require("../models/ImageGallery");
 
 // Api to get all categories
 route.get("/categories", function (req, res, next) {
@@ -17,39 +17,56 @@ route.get("/categories", function (req, res, next) {
 });
 
 /** API to find images by a category */
-/** API to filter image results http://localhost:3000/api/images/:categoryName?sortByDate=asc&sortByLikes=10 */
+/** API to filter image results http://localhost:3000/api/discover/images/:categoryName?sortByDate=asc&sortByLikes=10 */
 
-route.get("/images/:categoryName", function (req, res, next) {
-    const categoryName = req.params.categoryName;
-
-    /** get query parameters */
-    const queryParams = req.query;
-    console.log(queryParams);
-    const sortByDate = queryParams.sortByDate;
-    const sortByLikes = queryParams.sortByLikes;
-    // let parameters = new URLSearchParams(window.location.search);
-
-    const imageSet = [];
-    Image.find(
-        { category: categoryName },
-        { name: 1, category: 1 },
-        function (err, foundImages) {
-            if (err) {
-                console.log(err);
-            }
-            res.json(foundImages);
-            /** important to return res to the callback here itself and not outside find function */
+route.get("/images/:categoryName/:shuffle", async (req, res, next) => {
+    try {
+        const categoryName = req.params.categoryName;
+        const shuffle = req.params.shuffle;
+        /** get query parameters */
+        
+        const sortByDate = req.query.sortByDate;
+        const filterByLikes = req.query.filterByLikes;
+        let skip = parseInt(shuffle) || 0;
+        if(!categoryName) {
+            res.status(400).send("Bad request, check your sent parameters");
         }
-    ).limit(4);
-    // .sort(sortByDate)
-    // .sort(sortByLikes);
-});
 
-/** API to shuffle the image search results */
-route.get("/images/:category/:shuffle", function (req, res, next) {
-    const categoryName = req.params.categoryName;
-    const shuffle = req.params.shuffle;
-    const imageSet = [];
+        /** if the sort filter is given in request */
+		let sort = 1;
+        if(sortByDate) {
+            if(sortByDate == "asc") {
+                sort = 1;
+            } else if(sortByDate == "desc") {
+                sort = -1;
+            }
+        }
+		
+        /** if the filter for likes is given in request */
+        let filter = { };
+        if(filterByLikes) {
+            filter = { likes: filterByLikes }
+        }
+
+        // await can be used only if it's inside async function
+        const results = await ImageGallery.find(
+            { category: { $in :[categoryName] } , 
+            ...filter
+        }
+        ).sort( { createdAt: sort} )
+        .skip(skip)
+        .limit(4);
+
+        // send the result json
+		res.json(results);
+		
+
+    } catch (error) {
+        console.log(error);
+		next(error);
+    }
+    
 });
 
 module.exports = route;
+
